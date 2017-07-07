41120f371bfeaceee5a95f45ca6480310554e5f9
DocOch <Ochmanowicz@gmail.com>
2017-07-07 18:55:02 -0400

    fixed whitespace error in routes

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,18 +1,17 @@
-const axios = require("axios");
-var apiRouter = require('express').Router();
+const axios = require('axios');
+const router = require('express').Router();
 //search from Dice's API. 
-apiRouter.post('/search', function (req, res) {
-  var text = 'Java'; 
-  var city = "New+York,+NY"; //must be in this format; 
-	axios.get('http://service.dice.com/api/rest/jobsearch/v1/simple.json?text='+text+'&city='+city
-  )
-  .then(function(response) {     
-		res.send(response.data.resultItemList)
-	})
-	.catch(function(err) { 
-		console.warn(err);
-		res.send('NOPE')
-	})
+router.post('/search', (req, res) => {
+  const text = 'Java';
+  const city = 'New+York,+NY'; //must be in this format;
+  axios.get(`http://service.dice.com/api/rest/jobsearch/v1/simple.json?text={text}&city={city}`)
+    .then((response) => {
+      res.send(response.data.resultItemList);
+    })
+    .catch((err) => {
+      console.warn(err);
+      res.send('NOPE');
+    });
 });
 
-module.exports = apiRouter;
+module.exports = router;

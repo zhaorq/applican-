49b62549513abcf0e6365f90ea1f2ecaede80ee0
Tianyi Zhao <tianyi@Tianyis-MacBook-Air.local>
2017-07-07 11:14:14 -0400

    feat(api): set up api to Dice
    app.js /search post request to Dice API
    
    21

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const axios = require("axios");
 
 // INIT
 app.set('port', process.env.PORT || 2245);
@@ -11,6 +12,22 @@ app.listen(app.get('port'), (err) => {
     return console.log('Error starting server', err);
   }
   return console.log('Applican-: Listening on port:', app.get('port'));
+});
+
+
+//search from glassdoor's API. 
+app.post('/search', function (req, res) {
+	var text = 'Java'; 
+  var city = "New+York,+NY"; //must be in this format; 
+	axios.get('http://service.dice.com/api/rest/jobsearch/v1/simple.json?text='+text+'&city='+city
+  )
+  .then(function(response) {     
+		res.send(response.data.resultItemList)
+	})
+	.catch(function(err) { 
+		console.warn(err);
+		res.send('NOPE')
+	})
 });
 
 module.exports = app;

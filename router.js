5f7feb46327743db210ef62e5fa5fde5f8e4e052
Tianyi Zhao <tianyi@Tianyis-MacBook-Air.local>
2017-07-07 12:06:57 -0400

    refactor(Change API route): Added a router for API route

---
+++ router.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+var router = require('express').Router();
+//search from Dice's API. 
+router.post('/search', function (req, res) {
+  var text = 'Java'; 
+  var city = "New+York,+NY"; //must be in this format; 
+	axios.get('http://service.dice.com/api/rest/jobsearch/v1/simple.json?text='+text+'&city='+city
+  )
+  .then(function(response) {     
+		res.send(response.data.resultItemList)
+	})
+	.catch(function(err) { 
+		console.warn(err);
+		res.send('NOPE')
+	})
+});
+
+module.exports = router;

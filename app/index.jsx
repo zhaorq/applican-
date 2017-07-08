325a0e918e8e043733dfa6df5409def11454c176
Jason Lusthaus <jlusthaus@gmail.com>
2017-07-08 14:26:23 -0400

    feat(landing page): manage state of landing page with redux
    setup redux to manage state of the search input and results

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Provider } from 'react-redux';
 import Landing from './components/landing';
 import Search from './components/search';
-
-injectTapEventPlugin();
+import store from './store/store';
 
 const Index = () => (
-  <MuiThemeProvider>
+  <Provider store={store}>
     <Router>
       <switch>
         <Route exact path="/search" component={Search} />
         <Route exact path="/" component={Landing} />
       </switch>
     </Router>
-  </MuiThemeProvider>
-
+  </Provider >
 );
 
 

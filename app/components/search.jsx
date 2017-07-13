8238c1914270643590b1cc7a5eeed6b21a28dd11
Hhenry88 <henryhan88@outlook.com>
2017-07-13 15:36:25 -0400

    feat(job search): componentized job search
    componentized job search and passed it into landing.jsx and search.jsx. Fixed Dice API so job type
    is taken into consideration
    
    BREAKING CHANGE: Not breaking, but location doesn't work still. Location for dice API takes a
    zipcode for its location, and not city. So need to have a way to convert a cityname to a zipcode

--- app/components/search.jsx
+++ app/components/search.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import JobDetails from './jobDetailsComponent';
 import JobList from './jobList';
+import JobSearch from './jobSearchComponent';
 
-const Search = () => (
+const Search = props => (
   <div>
+    <JobSearch history={props.history} />
     <div className="mui-row">
       <div className="mui-col-md-5 mui-col-md-offset-1">
         <JobList />

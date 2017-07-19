e7ad84c41fe1a14b2ce926655569ac0de7eda8b6
DocOch <Ochmanowicz@gmail.com>
2017-07-19 13:52:18 -0400

    refactor(mc): mc

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,3 +1,3 @@
 export { updateJobListings, toggleJobListingStatus, updateJobStatusAPI, deleteJob, deleteJobAPI, setUserJobs, fetchUserJobs, setSortFilter } from './userActions';
 export { setUserAuth, fetchUserAuthStatus } from './authActions';
-export { setSearchTerm, selectJobDetails, setJobDesc } from './jobActions';
+export { setSearchTerm, selectJobDetails, setJobDesc, addContactApi, addContact } from './jobActions';

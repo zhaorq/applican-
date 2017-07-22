3aa19a4607340aebfedc2ddfd266d79a3d96c91e
DocOch <Ochmanowicz@gmail.com>
2017-07-22 15:31:14 -0400

    refactor(contact list table): reformate the view of contact list as a table

--- src/components/jobInProgress/ContactInfo.jsx
+++ src/components/jobInProgress/ContactInfo.jsx
@@ -11,19 +11,35 @@ export default class ContactInfo extends React.Component {
     axios.get('/api/contacts')
       .then((contactList) => {
         this.setState({ contactList });
+        console.log(this.state.contactList.data) // array
       });
   }
 
   render() {
     return (
       <div>
-        <p>{ Array.isArray(this.state.contactList.data) ? this.state.contactList.data.map((el) => {
-          return (
-            <div className="mui--divider-bottom">
-              <Contacts name={el.name} position={el.position} Email={el.Email} FollowUp={el.FollowUp} />
-            </div>
-          );
-        }) : 'no contacts' }</p>
+        <table style={{ border: '1px solid' }} >
+          <thead style={{ border: '1px solid' }}>
+            <tr>
+              <th style={{ border: '1px solid' }}>Name</th>
+              <th style={{ border: '1px solid' }}>Position</th>
+              <th style={{ border: '1px solid' }}>Email</th>
+              <th style={{ border: '1px solid' }}>FollowUp Date</th>
+            </tr>
+          </thead>
+          <tbody style={{ border: '1px solid' }}>
+            { Array.isArray(this.state.contactList.data) ? this.state.contactList.data.map((el) => {
+              return (
+                <tr style={{ border: '1px solid' }}>
+                  <td style={{ border: '1px solid' }}>{el.name}</td>
+                  <td style={{ border: '1px solid' }}>{el.position}</td>
+                  <td style={{ border: '1px solid' }}>{el.Email}</td>
+                  <td style={{ border: '1px solid' }}>{el.FollowUp}</td>
+                </tr>
+              );
+            }) : 'no contacts' }
+          </tbody>
+        </table>
       </div>
     );
   }

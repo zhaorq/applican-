2df5c8af3597faf752343f5f4807a90581c4358f
Jason Lusthaus <jlusthaus@gmail.com>
2017-07-25 10:42:49 -0400

    fix(store): fix bug preventing reading from local storage

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,11 @@ import rootReducer from '../reducers/rootReducer';
 
 
 /* eslint-disable */
+<<<<<<< HEAD
 
+=======
+const persistedState = localStorage.getItem('applican_store') ? JSON.parse(localStorage.getItem('applican_store')) : {};
+>>>>>>> fix(store): fix bug preventing reading from local storage
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
